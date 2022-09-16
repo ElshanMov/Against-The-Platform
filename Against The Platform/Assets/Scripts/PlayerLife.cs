4bc5116b0f2121d69b2e465736edf8d8855e954a
ElshanMov <62793862+ElshanMov@users.noreply.github.com>
2022-09-16 10:12:38 +0400

    Collectible Items Added

--- Against The Platform/Assets/Scripts/PlayerLife.cs
+++ Against The Platform/Assets/Scripts/PlayerLife.cs
@@ -6,24 +6,37 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
-    private void OnCollisionEnter(Collision collision)
+
+    bool dead = false;
+    private void Update()
     {
-        if (collision.gameObject.CompareTag("Enemy Body"))
+        if (transform.position.y < -1f && !dead)
         {
             Die();
         }
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy Body"))
+        {
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<PlayerMovement>().enabled = false;
+            Die();
+        }
+    }
 
-    
+
 
     private void Die()
     {
-        GetComponent<MeshRenderer>().enabled = false;
-        GetComponent<Rigidbody>().isKinematic = true;
-        GetComponent<PlayerMovement>().enabled = false;
+
         Invoke(nameof(ReloadLevel), 1.3f);
+        dead = true;
+
     }
-    void ReloadLevel() {
+    void ReloadLevel()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
